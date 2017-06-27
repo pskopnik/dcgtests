9c1952dfe6f45b9ec44a4450081652c4a2d7cd7a
Paul Skopnik <paul+github@skopnik.me>
2017-06-27 21:07:49 +0200

    Added interedges_cc test

---
+++ interedges_cc/test.cpp
@@ -0,0 +1,86 @@
+#include "test.h"
+
+#include <sstream>
+
+#include <NetworKit/generators/DynamicCommunitiesGenerator.h>
+#include <NetworKit/structures/Partition.h>
+
+
+void run(unsigned int k, unsigned int kOverL, double hhc) {
+		NetworKit::AffinityGenerator aGen;
+
+	auto m = aGen.halfHalf(k, hhc);
+
+	NetworKit::DynamicCommunitiesGenerator::Parameters parameters{m, k / kOverL, 1000, 0.5, 0.1};
+
+	NetworKit::DynamicCommunitiesGenerator gen(parameters);
+	NetworKit::GeneratorState state(gen);
+
+	#ifdef DYNAMICCOMMUNITIESGENERATOR_VALIDATOR
+	NetworKit::GeneratorValidator validator(gen);
+	validator.validate();
+	#endif /* DYNAMICCOMMUNITIESGENERATOR_VALIDATOR */
+
+	NetworKit::count intra, inter;
+
+
+	for (int i = 0; i < 10000; ++i) {
+		NetworKit::Partition p = gen.next();
+		auto clusters = state.getClusters();
+
+		intra = 0;
+		inter = 0;
+
+		for (auto clusterIt = clusters.cbegin(); clusterIt != clusters.cend(); ++clusterIt) {
+			for (auto it = clusterIt.cbegin(); it != clusterIt.cend(); ++it) {
+				for (auto postIt = it + 1; postIt != clusterIt.cend(); ++postIt) {
+					if (m.get(*it, *postIt) == 1) {
+						++intra;
+					} else {
+						++inter;
+					}
+				}
+			}
+		}
+
+		std::cout << static_cast<double>(inter) / static_cast<double>(inter + intra) << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char const *argv[]) {
+	unsigned int k, kOverL;
+	double hhc;
+
+	if (argc != 4) {
+		std::cerr << "Invalid number of arguments passed (expected 3): " << argc - 1 << std::endl;
+		return 1;
+	}
+
+	std::istringstream ss;
+
+	ss.str(std::string(argv[1]));
+	ss.clear();
+	if (!(ss >> k)) {
+		std::cerr << "Invalid value for k (expected int): " << argv[1] << std::endl;
+		return 1;
+	}
+
+	ss.str(std::string(argv[2]));
+	ss.clear();
+	if (!(ss >> kOverL)) {
+		std::cerr << "Invalid value for kOverL (expected int): " << argv[2] << std::endl;
+		return 1;
+	}
+
+	ss.str(std::string(argv[3]));
+	ss.clear();
+	if (!(ss >> hhc)) {
+		std::cerr << "Invalid value for hhc (expected double): " << argv[3] << std::endl;
+		return 1;
+	}
+
+	run(k, kOverL, hhc);
+
+	return 0;
+}
