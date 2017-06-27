ab5354ef0112eb2261f10e14bb2521b6d800c733
Paul Skopnik <paul+github@skopnik.me>
2017-06-27 21:39:10 +0200

    interedges_{cc,forest}: Added comment in header file

---
+++ interedges_cc/test.h
@@ -0,0 +1,27 @@
+#ifndef INTEREDGES_CC_H_
+#define INTEREDGES_CC_H_
+
+/**
+ * Tests the fraction of inter-edges in all connected components.
+ *
+ * For every connected component in the graph of chosen edges, the number of
+ * inter-edges between all pairs of nodes in the component are counted and
+ * the sum is divided by the total number of edges in the component.
+ *
+ * Output:
+ *  - Fraction of inter-edges for each timestep, separated by a space.
+ *  - One LF at the end of output.
+ *  - Output is given for 10000 timesteps.
+ *
+ * Further Parameters:
+ *  - Number of individuals, n = 1000.
+ *  - Home subcluster affinity, alpha = 0.5.
+ *  - Probability of subcluster change of an individual, p_move_v = 0.1.
+ *
+ * @param k Number of subclusters.
+ * @param kOverL The value of k/l, l is set from this and k: l := k / kOverL
+ * @param hhc Half-half coefficient. Passed to the AffinitiesGenerator.
+ */
+void run(unsigned int k, unsigned int kOverL, double hhc);
+
+#endif /* INTEREDGES_CC_H_ */
---
+++ interedges_forest/test.h
@@ -0,0 +1,28 @@
+#ifndef INTEREDGES_FOREST_H_
+#define INTEREDGES_FOREST_H_
+
+/**
+ * Tests the fraction of inter-edges in the forest.
+ *
+ * Checks the parent relationship of every subcluster. If it has a parent,
+ * the type of edge (inter- or intra-edge) is checked.
+ * Finally the total number inter-edges divided by the total number of edges
+ * is calculated.
+ *
+ * Output:
+ *  - Fraction of inter-edges for each timestep, separated by a space.
+ *  - One LF at the end of output.
+ *  - Output is given for 10000 timesteps.
+ *
+ * Further Parameters:
+ *  - Number of individuals, n = 1000.
+ *  - Home subcluster affinity, alpha = 0.5.
+ *  - Probability of subcluster change of an individual, p_move_v = 0.1.
+ *
+ * @param k Number of subclusters.
+ * @param kOverL The value of k/l, l is set from this and k: l := k / kOverL
+ * @param hhc Half-half coefficient. Passed to the AffinitiesGenerator.
+ */
+void run(unsigned int k, unsigned int kOverL, double hhc);
+
+#endif /* INTEREDGES_FOREST_H_ */
