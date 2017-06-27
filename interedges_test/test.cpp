7905e849d280117880950a3c0bb6797def706729
Paul Skopnik <paul+github@skopnik.me>
2017-06-27 15:18:45 +0200

    interedges_test: Added parameters for k, kOverL and hhc

--- interedges_test/test.cpp
+++ interedges_test/test.cpp
@@ -1,18 +1,17 @@
-
 #include "test.h"
+
+#include <sstream>
+
 #include <NetworKit/generators/DynamicCommunitiesGenerator.h>
 #include <NetworKit/structures/Partition.h>
 
 
-int main(int argc, char const *argv[])
-{
-	NetworKit::AffinityGenerator aGen;
+void run(unsigned int k, unsigned int kOverL, double hhc) {
+		NetworKit::AffinityGenerator aGen;
 
-	// auto m = aGen.halfHalf(6, 0.1);
-	auto m = aGen.halfHalf(30, 0.1);
+	auto m = aGen.halfHalf(k, hhc);
 
-	// NetworKit::DynamicCommunitiesGenerator::Parameters parameters{m, 3, 20, 0.5, 0.1};
-	NetworKit::DynamicCommunitiesGenerator::Parameters parameters{m, 6, 1000, 0.5, 0.1};
+	NetworKit::DynamicCommunitiesGenerator::Parameters parameters{m, k / kOverL, 1000, 0.5, 0.1};
 
 	NetworKit::DynamicCommunitiesGenerator gen(parameters);
 	NetworKit::GeneratorState state(gen);
@@ -44,6 +43,41 @@ int main(int argc, char const *argv[])
 		std::cout << static_cast<double>(inter) / static_cast<double>(inter + intra) << " ";
 	}
 	std::cout << std::endl;
+}
+
+int main(int argc, char const *argv[]) {
+	unsigned int k, kOverL;
+	double hhc;
+
+	if (argc != 4) {
+		std::cerr << "Invalid number of arguments passed (expected 3): " << argc - 1 << std::endl;
+		return 1;
+	}
+
+	std::istringstream ss;
+
+	ss.str(std::string(argv[1]));
+	ss.clear();
+	if (!(ss >> k)) {
+		std::cerr << "Invalid value for k (expected int): " << argv[1] << std::endl;
+		return 1;
+	}
+
+	ss.str(std::string(argv[2]));
+	ss.clear();
+	if (!(ss >> kOverL)) {
+		std::cerr << "Invalid value for kOverL (expected int): " << argv[2] << std::endl;
+		return 1;
+	}
+
+	ss.str(std::string(argv[3]));
+	ss.clear();
+	if (!(ss >> hhc)) {
+		std::cerr << "Invalid value for hhc (expected double): " << argv[3] << std::endl;
+		return 1;
+	}
+
+	run(k, kOverL, hhc);
 
 	return 0;
 }
