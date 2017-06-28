78aa9fedf6b4ba525cd3c7fe078250334cee03b2
Paul Skopnik <paul+github@skopnik.me>
2017-06-28 20:19:59 +0200

    Added cluster_owned test

---
+++ cluster_owned/test.cpp
@@ -0,0 +1,101 @@
+#include "test.h"
+#include "../shared/parts.h"
+
+#include <sstream>
+#include <vector>
+
+#include <NetworKit/generators/DynamicCommunitiesGenerator.h>
+#include <NetworKit/structures/Partition.h>
+
+
+void run(unsigned int k, unsigned int kOverL, double hhc) {
+	std::vector<std::vector<NetworKit::index>> parts;
+	NetworKit::AffinitiesGenerator aGen;
+
+	auto m = aGen.halfHalf(k, hhc, parts);
+
+	NetworKit::DynamicCommunitiesGenerator::Parameters parameters{m, k / kOverL, 1000, 0.5, 0.1};
+
+	NetworKit::DynamicCommunitiesGenerator gen(parameters);
+	NetworKit::GeneratorState state(gen);
+
+	#ifdef DYNAMICCOMMUNITIESGENERATOR_VALIDATOR
+	NetworKit::GeneratorValidator validator(gen);
+	validator.validate();
+	#endif /* DYNAMICCOMMUNITIESGENERATOR_VALIDATOR */
+
+	NetworKit::count encountered, owned;
+	std::vector<NetworKit::index> inverted = invert(parts);
+	std::vector<NetworKit::count> buckets(parts.size());
+
+	for (int i = 0; i < NO_OF_TIMESTEPS; ++i) {
+		NetworKit::Partition p = gen.next();
+		auto clusters = state.getClusters();
+
+		encountered = 0;
+		owned = 0;
+
+		for (auto clusterIt = clusters.cbegin() + 1; clusterIt != clusters.cend(); ++clusterIt) {
+
+			++encountered;
+			buckets.clear();
+			buckets.resize(parts.size(), 0);
+
+			for (auto it = (*clusterIt).cbegin(); it != (*clusterIt).cend(); ++it) {
+				++buckets[inverted[*it]];
+			}
+
+			for (auto it = buckets.cbegin(); it != buckets.cend(); ++it) {
+				if (*it > static_cast<double>((*clusterIt).size()) / 2) {
+					++owned;
+					break;
+				}
+			}
+		}
+
+		std::cout << static_cast<double>(owned) / static_cast<double>(encountered) << " ";
+	}
+
+	#ifdef DYNAMICCOMMUNITIESGENERATOR_VALIDATOR
+	validator.validate();
+	#endif /* DYNAMICCOMMUNITIESGENERATOR_VALIDATOR */
+
+	std::cout << std::endl;
+}
+
+int main(int argc, char const *argv[]) {
+	unsigned int k, kOverL;
+	double hhc;
+
+	if (argc != 4) {
+		std::cerr << "Invalid number of arguments passed (expected 3): " << argc - 1 << std::endl;
+		return 1;
+	}
+
+	std::istringstream ss;
+
+	ss.str(std::string(argv[1]));
+	ss.clear();
+	if (!(ss >> k)) {
+		std::cerr << "Invalid value for k (expected int): " << argv[1] << std::endl;
+		return 1;
+	}
+
+	ss.str(std::string(argv[2]));
+	ss.clear();
+	if (!(ss >> kOverL)) {
+		std::cerr << "Invalid value for kOverL (expected int): " << argv[2] << std::endl;
+		return 1;
+	}
+
+	ss.str(std::string(argv[3]));
+	ss.clear();
+	if (!(ss >> hhc)) {
+		std::cerr << "Invalid value for hhc (expected double): " << argv[3] << std::endl;
+		return 1;
+	}
+
+	run(k, kOverL, hhc);
+
+	return 0;
+}
---
+++ cluster_owned/test.h
@@ -0,0 +1,33 @@
+#ifndef CLUSTER_OWNED_H_
+#define CLUSTER_OWNED_H_
+
+#ifndef NO_OF_TIMESTEPS
+#define NO_OF_TIMESTEPS 5000
+#endif /* NO_OF_TIMESTEPS */
+
+/**
+ * Tests the fraction clearly owned clusters.
+ *
+ * That is, the fraction of clusters, which
+ *
+ * For every connected component in the graph of chosen edges, the number of
+ * inter-edges between all pairs of nodes in the component are counted and
+ * the sum is divided by the total number of edges in the component.
+ *
+ * Output:
+ *  - Fraction of clearly owned clusters for each timestep, separated by a space.
+ *  - One LF at the end of output.
+ *  - Output is given for NO_OF_TIMESTEPS timesteps.
+ *
+ * Further Parameters:
+ *  - Number of individuals, n = 1000.
+ *  - Home subcluster affinity, alpha = 0.5.
+ *  - Probability of subcluster change of an individual, p_move_v = 0.1.
+ *
+ * @param k Number of subclusters.
+ * @param kOverL The value of k/l, l is set from this and k: l := k / kOverL
+ * @param hhc Half-half coefficient. Passed to the AffinitiesGenerator.
+ */
+void run(unsigned int k, unsigned int kOverL, double hhc);
+
+#endif /* CLUSTER_OWNED_H_ */
